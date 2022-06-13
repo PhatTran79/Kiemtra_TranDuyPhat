9fc69ff96edd4e0500b2e1509150d0fe8e1548eb
phatl <phatl@LAPTOP-FAT>
2022-06-13 13:20:27 +0700

    Add project files.

---
+++ Kiemtra_TranDuyPhat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kiemtra_TranDuyPhat.MvcApplication" Language="C#" %>
---
+++ Kiemtra_TranDuyPhat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kiemtra_TranDuyPhat.Startup))]
+namespace Kiemtra_TranDuyPhat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
